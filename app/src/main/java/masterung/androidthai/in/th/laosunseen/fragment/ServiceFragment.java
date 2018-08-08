4a76cfdc8b998712b163650841ef2b544dba74d3
masterUNG <phrombutr@gmail.com>
2018-08-08 15:46:33 +0700

    Upload OK3

---
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/ServiceFragment.java
@@ -0,0 +1,38 @@
+package masterung.androidthai.in.th.laosunseen.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.laosunseen.MainActivity;
+import masterung.androidthai.in.th.laosunseen.R;
+
+public class ServiceFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarService);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_service, container, false);
+        return view;
+    }
+}
